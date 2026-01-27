9bb13d38048c2b8f339d5721dc52c6f9ed19e647
Fabian Orccon <cfoch.fabian@gmail.com>
2026-01-27 15:18:17 +0100

    Revert "av: auddec: Enable wasm support"
    This reverts commit f181a17a53df2d3d5394f8de012968c09cc23e1c.

--- subprojects/gst-libav/ext/libav/gstavauddec.c
+++ subprojects/gst-libav/ext/libav/gstavauddec.c
@@ -41,10 +41,8 @@ GST_DEBUG_CATEGORY_STATIC (GST_CAT_PERFORMANCE);
 
 /* A number of function prototypes are given so we can refer to them later. */
 static void gst_ffmpegauddec_base_init (GstFFMpegAudDecClass * klass);
-static void gst_ffmpegauddec_class_init (GstFFMpegAudDecClass * klass,
-    gconstpointer class_data);
-static void gst_ffmpegauddec_init (GstFFMpegAudDec * ffmpegdec,
-    GTypeClass g_class);
+static void gst_ffmpegauddec_class_init (GstFFMpegAudDecClass * klass);
+static void gst_ffmpegauddec_init (GstFFMpegAudDec * ffmpegdec);
 static void gst_ffmpegauddec_finalize (GObject * object);
 static gboolean gst_ffmpegauddec_propose_allocation (GstAudioDecoder * decoder,
     GstQuery * query);
@@ -122,8 +120,7 @@ gst_ffmpegauddec_base_init (GstFFMpegAudDecClass * klass)
 }
 
 static void
-gst_ffmpegauddec_class_init (GstFFMpegAudDecClass * klass,
-    gconstpointer class_data)
+gst_ffmpegauddec_class_init (GstFFMpegAudDecClass * klass)
 {
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
   GstAudioDecoderClass *gstaudiodecoder_class = GST_AUDIO_DECODER_CLASS (klass);
@@ -146,7 +143,7 @@ gst_ffmpegauddec_class_init (GstFFMpegAudDecClass * klass,
 }
 
 static void
-gst_ffmpegauddec_init (GstFFMpegAudDec * ffmpegdec, GTypeClass g_class)
+gst_ffmpegauddec_init (GstFFMpegAudDec * ffmpegdec)
 {
   GST_PAD_SET_ACCEPT_TEMPLATE (GST_AUDIO_DECODER_SINK_PAD (ffmpegdec));
   gst_audio_decoder_set_use_default_pad_acceptcaps (GST_AUDIO_DECODER_CAST
