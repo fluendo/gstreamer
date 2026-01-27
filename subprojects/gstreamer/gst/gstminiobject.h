231f924adf0835698dad03fbba9f500098a84236
Fabian Orccon <cfoch.fabian@gmail.com>
2026-01-27 15:18:23 +0100

    Revert "gstbin: Fix bad function pointer"
    This reverts commit c493109acbc1a9aff703de6e805689480d4493ed.

--- subprojects/gstreamer/gst/gstminiobject.h
+++ subprojects/gstreamer/gst/gstminiobject.h
@@ -297,11 +297,6 @@ gboolean        gst_mini_object_take            (GstMiniObject **olddata, GstMin
 GST_API
 GstMiniObject * gst_mini_object_steal           (GstMiniObject **olddata) G_GNUC_WARN_UNUSED_RESULT;
 
-#ifdef __EMSCRIPTEN__
-GST_API
-GstMiniObject * gst_mini_object_ref_with_data   (GstMiniObject *mini_object, gpointer data);
-#endif
-
 /**
  * GST_DEFINE_MINI_OBJECT_TYPE:
  * @TypeName: name of the new type in CamelCase
