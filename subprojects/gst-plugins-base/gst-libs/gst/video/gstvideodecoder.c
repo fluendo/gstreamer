cfad627bd17d4c909638549df5bb7f7d20bd157c
Fabian Orccon <cfoch.fabian@gmail.com>
2026-01-27 15:22:01 +0100

    Revert "Fix signatures"
    This reverts commit 3c1858c88cabe4fb565b623abc035306031a2561.

--- subprojects/gst-plugins-base/gst-libs/gst/video/gstvideodecoder.c
+++ subprojects/gst-plugins-base/gst-libs/gst/video/gstvideodecoder.c
@@ -469,8 +469,7 @@ static gint private_offset = 0;
 #define META_TAG_VIDEO meta_tag_video_quark
 static GQuark meta_tag_video_quark;
 
-static void gst_video_decoder_class_init (GstVideoDecoderClass * klass,
-    gpointer klass_data);
+static void gst_video_decoder_class_init (GstVideoDecoderClass * klass);
 static void gst_video_decoder_init (GstVideoDecoder * dec,
     GstVideoDecoderClass * klass);
 
@@ -586,7 +585,7 @@ gst_video_decoder_get_instance_private (GstVideoDecoder * self)
 }
 
 static void
-gst_video_decoder_class_init (GstVideoDecoderClass * klass, gpointer klass_data)
+gst_video_decoder_class_init (GstVideoDecoderClass * klass)
 {
   GObjectClass *gobject_class;
   GstElementClass *gstelement_class;
